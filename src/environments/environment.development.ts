2cd9b4298fb9e8007ed269c0d31a52955d1d662b
Ajnas T <ajnasthayyil123@gmail.com>
2026-06-12 18:23:42 +0530

    fix: update production api url to correct azure backend deployment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://shoex-api.azurewebsites.net/api'
+  apiUrl: 'https://shoex-api-exf4f9dxfng0gne9.southindia-01.azurewebsites.net/api'
 };
